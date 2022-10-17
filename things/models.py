14ace1569b583b8e0045c66f954cbba7c950bb26
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-17 13:16:06 +0100

    fix error

--- things/models.py
+++ things/models.py
@@ -5,8 +5,8 @@ from enum import unique
 
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique = True, blank = False),
-    description = models.CharField(max_length =120, blank =True),
-    quantity = models.IntegerField(unique = False, validators= [MinValueValidator(0),MaxValueValidator(100)] )
+    name = models.CharField(max_length=30, unique = True, blank = False)
+    description = models.CharField(max_length =120, blank =True)
+    quantity = models.IntegerField(validators= [MinValueValidator(0),MaxValueValidator(100)] )
     
                                        
