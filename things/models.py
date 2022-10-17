5ad0083723a51eb3d91ff692e69ae60dcf900904
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-17 12:53:54 +0100

    exrcise 3

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ from enum import unique
 
 
 class Thing(models.Model):
-    name = models.TextField(max_length=30, unique = True, blank = False)
-    description = models.TextField(max_length =120,blank =True)
-    quantity = models.TextField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(100)])
+    name = models.CharField(max_length=30, unique = True, blank = False)
+    description = models.CharField(max_length =120,blank =True)
+    quantity = models.IntegerField(validators= [MinValueValidator(0),MaxValueValidator(100)] )
+                                       
